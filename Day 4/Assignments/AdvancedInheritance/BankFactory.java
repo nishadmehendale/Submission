a17d077ce2e2673214056da0925db7509f23c6d9
nishadmehendale <nishadmehendale@gmail.com>
2018-07-29 12:06:48 +0530

    Update and rename 4.1BankFactory.java to BankFactory.java

--- Day 4/Assignments/AdvancedInheritance/BankFactory.java
+++ Day 4/Assignments/AdvancedInheritance/BankFactory.java
@@ -1,4 +1,4 @@
-
+//Class BankFactory having abstract methods for creating SavingsAcc and CurrentAcc
 public abstract class BankFactory {
 	public abstract SavingAcc getNewSavingAcc(int accNo, String accNm, int accBal, boolean isSalaried);
 	public abstract CurrentAcc getNewCurrentAcc(int accNo, String accNm, int accBal, float creditLimit);
