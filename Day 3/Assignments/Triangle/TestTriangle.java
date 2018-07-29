0ee045df5d87084f18019ebec9888fc68a61b02d
nishadmehendale <nishadmehendale@gmail.com>
2018-07-29 11:52:13 +0530

    Update TestTriangle.java

--- Day 3/Assignments/Triangle/TestTriangle.java
+++ Day 3/Assignments/Triangle/TestTriangle.java
@@ -1,30 +1,36 @@
-import java.util.Scanner;
 import java.util.*;
-//Class TestTriangle that tests functionalities of Class Triangle
+//Class TestTriangle that tests class Triangle and displays triangle type
 public class TestTriangle {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//Accepting Input
+		
+		//Accepting input
 		System.out.println("Enter number of triangles");
 		int num = sc.nextInt();
+		int sides[] = new int[3];
 		Triangle[] triangle = new Triangle[num];
-		for(int i=0;i<num;i++) {
+		for(int i=0;i<num;i++){
 			triangle[i] = new Triangle();
-			System.out.println("Enter length of Side 1");
-			double side1 = sc.nextDouble();
-			System.out.println("Enter length of Side 2");
-			double side2 = sc.nextDouble();
-			System.out.println("Enter length of Side 3");
-			double side3 = sc.nextDouble();
-			triangle[i].setSide1(side1);
-			triangle[i].setSide2(side2);
-			triangle[i].setSide3(side3);
-		}
-		//Checking the type of each triangle
-		int type[] = new int[num];
-		type = triangle[0].checkType(triangle);
+			System.out.println("Enter the sides");
+			sides[0] = sc.nextInt();
+			sides[1] = sc.nextInt();
+			sides[2] = sc.nextInt();
+			triangle[i].setSide(sides);
+			System.out.println(triangle[i]);
+			if(triangle[i].isRight(triangle[i])){
+				System.out.println("Triangle is right angled");
+			}
+			if(triangle[i].isScalene(triangle[i])){
+				System.out.println("Triangle is scalene");
+			}
+			else if(triangle[i].isEquilateral(triangle[i])){
+				System.out.println("Triangle is equilateral");
+			}
+			else if(triangle[i].isIsosceles(triangle[i]){
+				System.out.println("Triangle is isosceles");
+			}
+		}			
 		
 	}
-
 }
