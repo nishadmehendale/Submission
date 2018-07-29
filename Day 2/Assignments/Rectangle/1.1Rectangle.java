cf1abe0daa9af1c968a032f1f8f8f934c8b7834d
nishadmehendale <nishadmehendale@gmail.com>
2018-07-29 10:54:33 +0530

    Update 1.1Rectangle.java

--- Day 2/Assignments/Rectangle/1.1Rectangle.java
+++ Day 2/Assignments/Rectangle/1.1Rectangle.java
@@ -1,34 +1,43 @@
-//
-public class Rectangle {
+//Class rectangle that has length and breadth and calculates area and circumference
+	public class Rectangle {
 	private float length;
 	private float breadth;
+
 	public float getLength() {
-		return length;
+	return length;
 	}
+
 	public void setLength(float length) {
-		if(length > 0.0f)
-			this.length = length;
+	if(length > 0.0f)
+	this.length = length;
 	}
+
 	public float getBreadth() {
-		return breadth;
+	return breadth;
 	}
+
 	public void setBreadth(float breadth) {
-		if(breadth > 0.0f) 
-			this.breadth = breadth;
+	if(breadth > 0.0f)
+	this.breadth = breadth;
 	}
+
+	//Method to calculate area
 	public float area()
 	{
-		float area = length * breadth;
-		return area;
+	float area = length * breadth;
+	return area;
 	}
+
+	//method to calculate circumference
 	public float circumference()
 	{
-		float circumference = 2 * (length + breadth);
-		return circumference;
+	float circumference = 2 * (length + breadth);
+	return circumference;
 	}
+
 	@Override
 	public String toString() {
-		return "Rectangle [length=" + length + ", breadth=" + breadth + ", area()=" + area() + ", circumference()="
-				+ circumference() + "]";
+	return "Rectangle [length=" + length + ", breadth=" + breadth + ", area()=" + area() + ", circumference()="
+	+ circumference() + "]";
 	}
-}
+	}
